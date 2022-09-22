accaa2954ca3377930a692599916936dc7866472
Adil Shamuratov <iswearican_1@outlook.com>
2022-09-22 18:25:26 +0600

    f

--- OneDrive/Рабочий стол/New folder/back_zeon_charity/ZeonCharity/ZeonCharity/settings.py
+++ OneDrive/Рабочий стол/New folder/back_zeon_charity/ZeonCharity/ZeonCharity/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+# import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +29,7 @@ SECRET_KEY = 'django-insecure-6l$+py_km=&echz%&8v6thw60v(mf6#dvajahgc#2u*7n5&@mi
 DEBUG = True
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # auth conf
 AUTHENTICATION_BACKENDS = ['user.models.EmailBackend']
@@ -125,8 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# django_heroku.settings(locals())
