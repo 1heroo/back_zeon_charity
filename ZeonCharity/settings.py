dd75a0ad39a86990873c23f04609c8adf5318ae5
renren-017 <altynai.mamytova17@gmail.com>
2022-09-23 14:03:33 +0600

    adding jazzmin

--- ZeonCharity/settings.py
+++ ZeonCharity/settings.py
@@ -45,6 +45,7 @@ AUTH_USER_MODEL = 'user.MyUser'
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
